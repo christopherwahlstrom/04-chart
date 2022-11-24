07bf0abe6737ec3480aa1541ce100f165ac88153
David Andersson <david.andersson@zocom.se>
2022-11-24 14:36:05 +0100

    Register components (tree shaking)

---
+++ src/components/Demo.tsx
@@ -0,0 +1,13 @@
+import Chart from 'chart.js/auto';
+import { Pie, Bar, Line } from 'react-chartjs-2';
+import { data1, data2 } from './demoData'
+
+
+const DemoChart = () => (
+	<div className="demo-chart">
+		<Line data={data1} />
+		{/* <Bar data={data2} /> */}
+	</div>
+)
+
+export default DemoChart
